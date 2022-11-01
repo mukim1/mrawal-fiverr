50cc87e8c61363949f5c46cf618f6a8df08724b9
unknown <shajibhasan7325@gmail.com>
2022-11-01 21:40:33 +0600

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Button from "../SmComponent/Button";
+import Image from "next/image";
 import {
   AiFillFacebook,
   AiFillInstagram,
@@ -12,27 +13,48 @@ import { Accordion } from "@mantine/core";
 
 const Footer = () => {
   return (
-    <div className="bg-black md:p-14">
-      <div className="hidden text-white md:grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
-        <div className="hidden col-span-2 md:block md:col-span-1">
-          <h2 className="mb-10 text-5xl font-bold">lano</h2>
-          <p className="mb-2">Imprint</p>
-          <p>Privacy Policy</p>
-        </div>
-
-        {data.map((v) => (
-          <div className="" key={v.title}>
-            <Link href={"/"}>
-              <a className="block mb-3 text-xl font-semibold">{v.title}</a>
-            </Link>
-
-            {v.item.map((v2) => (
-              <p className="py-2 text-sm " key={v2.item}>
-                {v2.item}
-              </p>
-            ))}
+    <div className="bg-[#004650] md:p-14">
+      <div className="hidden text-white md:grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-3 lg:gap-10">
+        <div className="hidden col-span-2 md:block md:col-span-1 ">
+          <Image src={`/assets/logo2.png`} alt="" width={100} height={30} />
+          <p className="mt-5 text-lg">
+            Omni HR helps companies automate their end-to-end employee journey
+            including recruitment, onboarding, time off, employee management,
+            and more.
+          </p>
+          {/* <p className="mb-2">Imprint</p>
+          <p>Privacy Policy</p> */}
+          <div className="flex gap-8 mt-5 ">
+            <div className="p-2 text-gray-300 rounded-full bg-slate-500 hover:text-white">
+              <AiFillTwitterCircle size={25} />
+            </div>
+            <div className="p-2 text-gray-300 rounded-full bg-slate-500 hover:text-white">
+              <AiFillFacebook size={25} />
+            </div>
+            <div className="p-2 text-gray-300 rounded-full bg-slate-500 hover:text-white">
+              <AiFillLinkedin size={25} />
+            </div>
+            <div className="p-2 text-gray-300 rounded-full bg-slate-500 hover:text-white">
+              <AiFillInstagram size={25} />
+            </div>
           </div>
-        ))}
+        </div>
+        {/*  */}
+        <div className="grid grid-cols-3 col-span-2 ">
+          {data.map((v) => (
+            <div className="" key={v.title}>
+              <Link href={"/"}>
+                <a className="block mb-3 text-xl font-semibold">{v.title}</a>
+              </Link>
+
+              {v.item.map((v2) => (
+                <p className="py-2 text-sm " key={v2.item}>
+                  {v2.item}
+                </p>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="md:hidden">
@@ -55,7 +77,7 @@ const Footer = () => {
       </div>
 
       {/* BOTTON BAR */}
-      <div className="flex items-center justify-between mt-20 ">
+      {/* <div className="flex items-center justify-between mt-20 ">
         <div className="flex flex-col gap-3 md:flex-row">
           <button className="px-5 py-2 text-white bg-gray-500 rounded-lg hover:underline">
             We’re hiring{" "}
@@ -83,13 +105,13 @@ const Footer = () => {
             className=" text-gray-300 hover:text-[#FFFFFF]"
           />
         </div>
-      </div>
-      <div className="flex items-center justify-between text-white mt-14 ">
+      </div> */}
+      <div className="text-white mt-14">
         <p className="">© Lano Software GmbH 2022</p>
-        <div className="flex gap-2 ">
+        {/* <div className="flex gap-2 ">
           <AiOutlineGlobal size={20} />
           <p>English</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
